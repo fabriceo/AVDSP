15fa1554c6c75e1d69d28c49db9a816e2ca1beea
TLeconte <thierry.leconte35@free.fr>
2020-02-08 12:06:36 +0100

    fix for oktoprog as shard lib

--- module_avdsp/dspprogs/oktoprog.c
+++ module_avdsp/dspprogs/oktoprog.c
@@ -1,11 +1,5 @@
-
-/*
- * this program is for testing the encode with a user dsp program
- * can be compiled directly on any platfform with gcc (install mingw64 for windows)
- * launch make -f dspcreate.mak all
- *
- */
-
+#include <stdlib.h>
+#include <string.h>
 #include "dsp_encoder.h"
 
 #define DACOUT(x) (x)
@@ -13,15 +7,11 @@
 #define USBOUT(x) (16 + x)
 #define USBIN(x)  (16 + 8 + x)
 
-int oktoProg(){
+int oktoProg(int fcross, int subdelay){
     dsp_PARAM();
 
     int lowpass = dspBiquad_Sections(3);
         dsp_LP_BES6(fcross);
-        //dsp_Filter2ndOrder(FPEAK,1000, 0.5, 1.0);
-        //dsp_Filter2ndOrder(FPEAK,1000, 0.5, 1.0);
-    int highpass = dspBiquad_Sections(2);
-        dsp_HP_LR4(fcross);
         //dsp_Filter2ndOrder(FPEAK,1000, 0.5, 1.0);
         //dsp_Filter2ndOrder(FPEAK,1000, 0.5, 1.0);
 
@@ -44,38 +34,30 @@ int oktoProg(){
     return dsp_END_OF_CODE();
 }
 
-int oktoProg2(){
-    dsp_PARAM();
-
-    int lowpass = dspBiquad_Sections(2);
-        dsp_LP_BES6(1000.0);
-
-
-    dsp_CORE();  // first core
-
-    dsp_LOAD(USBOUT(0));
-    dsp_COPYXY();
-    dsp_DELAY_FixedMicroSec(200);
-    dsp_SWAPXY();
-    dsp_BIQUADS(lowpass);
-    dsp_STORE(DACOUT(0));
-    dsp_SWAPXY();
-    dsp_SUBXY();
-    dsp_STORE(DACOUT(1));
-
-    dsp_LOAD(USBOUT(1));
-    dsp_STORE(USBIN(1));
-
-    dsp_LOAD(ADCIN(0));
-    dsp_STORE(USBIN(0));
-
-    dsp_LOAD(ADCIN(1));
-    dsp_STORE(USBIN(2));
-    return dsp_END_OF_CODE();
-}
-
 
 int dspProg(int argc,char **argv){
-    return oktoProg();
+   int fcross = 1000;  
+   int subdelay = 745; 
+
+   for(int i=0 ; i<argc;i++) {
+        // parse USER'S command line parameters
+
+                 if (strcmp(argv[i],"-fx") == 0) {
+                     i++;
+                     if (argc>i) {
+                         fcross = strtol(argv[i], NULL,10);
+                         printf("crossover fx = %d\n",fcross);
+                         continue; } }
+
+                 if (strcmp(argv[i],"-subdelay") == 0) {
+                     i++;
+                     if (argc>i) {
+                         subdelay = strtol(argv[i], NULL,10);
+                         printf("subdelay = %d\n",subdelay);
+                         continue; } }
+
+        }
+
+    return oktoProg(fcross,subdelay);
 }
 
