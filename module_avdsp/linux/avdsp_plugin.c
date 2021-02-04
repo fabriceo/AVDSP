87ddbee8197d8377cefd7a215a9725563e9ff10f
fabriceo <fabrice.oudert@gmail.com>
2021-02-04 11:54:18 +0100

    update avdsp_plugin.c

--- module_avdsp/linux/avdsp_plugin.c
+++ module_avdsp/linux/avdsp_plugin.c
@@ -130,6 +130,7 @@ dsp_transfer(snd_pcm_extplug_t *ext,
 	            int out = dsp->coreio[nc].outputMap[ch];
 	            int sample = inputOutput[ out ];
 	            // add a tag that can be recognized easily to verify bitperfect, kind of dithering approach
+	            if ((dsp->samplestotal & 0x00040000) == 0)
 	            if ((ch == 0) && (dsp->tagoutput)) {
 	                int newsample = sample & 0xFFFFF000;
 	                sample = newsample | (dsp->previoussample & 0x00000F00);
@@ -270,7 +271,7 @@ SND_PCM_PLUGIN_DEFINE_FUNC(avdsp)
         if (strcmp(id, "tagoutput") == 0) {
             long val;
             if(snd_config_get_integer(n,&val)==0) {
-                if ((val >= 15) && (val <= 31)) {
+                if ((val >= 0) && (val <= 31)) {
                     dsp->tagoutput = val;
                     dsp->tagmask = 0xFFFFFFFF << (32-val);
                     continue;
