830c3dddaeaae9584a6173e3f4cdc09c426868a4
fabriceo <fabrice.oudert@gmail.com>
2025-01-15 18:48:59 +0100

    dspcreate augmented

--- module_avdsp/encoder/dsp_basiccreate.c
+++ module_avdsp/encoder/dsp_basiccreate.c
@@ -540,7 +540,6 @@ int dspbasicCreate(char * dspbasicName, int argc, char **argv){
     char * nextName = dspbasicName;
     int size    = 0;
     lineNum = 0;
-    int numCore = 1;
     while (nextName && (*nextName)) {
 
     dspbasicName = nextName;
