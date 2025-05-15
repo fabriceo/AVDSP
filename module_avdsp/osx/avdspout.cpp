3eb7d9b4ac01aa30a4a3878778c6141555b4017f
fabriceo <fabrice.oudert@gmail.com>
2025-05-15 20:31:55 +0200

    osx

--- module_avdsp/osx/avdspout.cpp
+++ module_avdsp/osx/avdspout.cpp
@@ -3,12 +3,25 @@
 #include "avdspinclude.hpp"
 
 
+// dsp_PARAM section start
+const float lp[][6] = {
+   { 51, 400.000000, 1.000000, 1.000000, 0, 0 }, //LPLR4
+   { 0, 0, 0, 0, 0, 0 },
+{ 0,0,0,0,0,0},{ 0,0,0,0,0,0}, }; //2 biquad cell(s) provided
+const float hp[][6] = {
+   { 59, 400.000000, 1.000000, 1.000000, 0, 0 }, //HPLR4
+   { 0, 0, 0, 0, 0, 0 },
+{ 0,0,0,0,0,0},{ 0,0,0,0,0,0}, }; //2 biquad cell(s) provided
+const float lpbe8[][6] = {
+   { 7, 400.000000, 1.000000, 1.000000, 0, 0 }, //LPBE8
+   { 0, 0, 0, 0, 0, 0 },
+   { 0, 0, 0, 0, 0, 0 },
+{ 0,0,0,0,0,0},{ 0,0,0,0,0,0},{ 0,0,0,0,0,0},{ 0,0,0,0,0,0}, }; //4 biquad cell(s) provided
+const float hp2[][6] = {
+   { 67, 400.000000, 0.770000, 1.000000, 0, 0 }, //HP2
+{ 0,0,0,0,0,0}, }; //1 biquad cell(s) provided
 void dsp_CORE1() {
    if (0==dsp_CORE(0xffffffff,0x0)) return;
-   dsp_LOAD_STORE(0,0); //TODO missing parameters
-   dsp_LOAD(16);
-   dsp_VALUEX(0.500000);
-   dsp_GAIN(0.500000);
-   dsp_STORE(25);
+   dsp_SINE(1000,1.000000);
 } //end of core 1
-int dspDataSpace1[32];
+int dspDataSpace1[36];
