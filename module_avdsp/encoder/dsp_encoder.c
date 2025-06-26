4cc0b31a60f40dbfaa458c24498e05bbc948f305
fabriceo <fabrice.oudert@gmail.com>
2025-06-26 14:31:26 +0200

    delay with 0

--- module_avdsp/encoder/dsp_encoder.c
+++ module_avdsp/encoder/dsp_encoder.c
@@ -1696,9 +1696,9 @@ static void dsp_DELAY_FixedMicroSec_(int microSec, int opcode){
     int fshi = dspConvertFrequencyFromIndex(dspMaxSamplingFreq);
     int fslo = dspConvertFrequencyFromIndex(dspMinSamplingFreq);
     dspprintf2("    DELAY %dus -> %d samples @%d -> %.0fus. @%d -> %.0fus\n",microSec,maxSamples,fshi,(float)maxSamples / (float)fshi * 1000000.0,fslo,(float)minSamples / (float)fslo * 1000000.0);
-    addCode(microSec);  // store the expected delay in uSec
-    int data;
     if (maxSamples) {
+        addCode(microSec);  // store the expected delay in uSec
+        int data;
         if (DP == 1 ) {
             data = addDataSpace(1 + maxSamples); // request data space (including index) and store the pointer
             if (opcode == DSP_DELAY) dspout("   dsp_DELAY(%d,%d,%d);\n",microSec,data,maxSamples);
@@ -1707,7 +1707,9 @@ static void dsp_DELAY_FixedMicroSec_(int microSec, int opcode){
             data = addDataSpaceMisAligned8(1 + maxSamples*2);
             dspout("   dsp_DELAY_DP(%d,%d,%d);\n",microSec,data,maxSamples);
         }
-    } else addCode(0);
+    } else {
+        addCode(0);addCode(0);
+    }
     addCode(0); // this will indicate to runtime that this is a fixed delay line.
 }
 
